be42ac209532bf77c99523af99719eef7362ebb0
hayaboosan <hayaboosan@gmail.com>
2021-03-23 19:34:57 +0900

    [Change]ログイン状態によってrootを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#new'
+  constraints ->(request) { request.session[:user_id].present? } do
+    root 'posts#index'
+  end
 
-  resources :users, only: %i[new create]
+  root 'user_sessions#new'
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
+  resources :posts
 end

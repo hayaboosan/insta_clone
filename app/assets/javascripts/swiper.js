4419ff5e7b2dd2f5ead8e3c8e688620fd8bb2d95
hayaboosan <hayaboosan@gmail.com>
2021-03-27 09:55:57 +0900

    [Fix]swiperが動作しない不具合を修正

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -1,1 +1,15 @@
-<script>var swiper = new Swiper('.swiper-container');</script>;
+$(function() {
+  var mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })  
+})

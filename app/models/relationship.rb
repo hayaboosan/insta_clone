62f6e72a69ec15fb581b4cedb17e13aadc7f0af1
hayaboosan <hayaboosan@gmail.com>
2021-04-10 12:09:33 +0900

    [Fix]relationshipモデルのミス

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -19,5 +19,5 @@ class Relationship < ApplicationRecord
   belongs_to :followed, class_name: 'User'
   validates :follower_id, presence: true
   validates :followed_id, presence: true
-  validates :follower_id, uniqueness: { scope: :followerd_id }
+  validates :follower_id, uniqueness: { scope: :followed_id }
 end

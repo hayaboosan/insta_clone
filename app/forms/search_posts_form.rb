eee1c790cc45cbbbc05ddde3471b5b6ba135488a
hayaboosan <hayaboosan@gmail.com>
2021-04-12 19:56:40 +0900

    [Change]コメント及びユーザー名検索欄を追加

--- app/forms/search_posts_form.rb
+++ app/forms/search_posts_form.rb
@@ -4,10 +4,22 @@ class SearchPostsForm
 
   attribute :body, :string
   attribute :comment_body, :string
+  attribute :username, :string
 
   def search
     scope = Post.distinct
-    scope = scope.body_contain(body) if body.present?
+    if body.present?
+      scope = splited_bodies.map { |splited_body| scope.body_contain(splited_body) }
+                            .inject { |result, scp| result.or(scp) }
+    end
+    scope = scope.comment_body_contain(body) if comment_body.present?
+    scope = scope.username_contain(username) if username.present?
     scope
+  end
+
+  private
+
+  def splited_bodies
+    body.strip.split(/[[:blank:]]+/)
   end
 end

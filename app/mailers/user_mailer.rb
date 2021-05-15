8fddf5ece7e5567406f9e4f91fe00e166af6fb7a
hayaboosan <hayaboosan@gmail.com>
2021-05-15 13:11:37 +0900

    [Fix]スペルミス修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
   def like_post
-    @user_form = params[:user_from]
+    @user_from = params[:user_from]
     @user_to = params[:user_to]
     @post = params[:post]
-    mail(to: @user_email, subject: "#{@user_from.username}があなたの投稿にいいねしました")
+    mail(to: @user_to.email, subject: "#{@user_from.username}があなたの投稿にいいねしました")
   end
 
   def comment_post

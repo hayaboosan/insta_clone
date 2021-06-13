2cf82b1e0c976a8a7e66f8b392d3c91d41b78de2
hayaboosan <hayaboosan@gmail.com>
2021-06-13 13:55:27 +0900

    [Change]モデル名変更join_chatroomからchatroom_users

--- app/models/chatroom_user.rb
+++ app/models/chatroom_user.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Table name: join_chatrooms
+# Table name: chatroom_users
 #
 #  id          :bigint           not null, primary key
 #  created_at  :datetime         not null
@@ -10,16 +10,16 @@
 #
 # Indexes
 #
-#  index_join_chatrooms_on_chatroom_id              (chatroom_id)
-#  index_join_chatrooms_on_user_id                  (user_id)
-#  index_join_chatrooms_on_user_id_and_chatroom_id  (user_id,chatroom_id) UNIQUE
+#  index_chatroom_users_on_chatroom_id              (chatroom_id)
+#  index_chatroom_users_on_user_id                  (user_id)
+#  index_chatroom_users_on_user_id_and_chatroom_id  (user_id,chatroom_id) UNIQUE
 #
 # Foreign Keys
 #
 #  fk_rails_...  (chatroom_id => chatrooms.id)
 #  fk_rails_...  (user_id => users.id)
 #
-class JoinChatroom < ApplicationRecord
+class ChatroomUser < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
 end
--- spec/factories/chatroom_users.rb
+++ spec/factories/chatroom_users.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Table name: join_chatrooms
+# Table name: chatroom_users
 #
 #  id          :bigint           not null, primary key
 #  created_at  :datetime         not null
@@ -10,9 +10,9 @@
 #
 # Indexes
 #
-#  index_join_chatrooms_on_chatroom_id              (chatroom_id)
-#  index_join_chatrooms_on_user_id                  (user_id)
-#  index_join_chatrooms_on_user_id_and_chatroom_id  (user_id,chatroom_id) UNIQUE
+#  index_chatroom_users_on_chatroom_id              (chatroom_id)
+#  index_chatroom_users_on_user_id                  (user_id)
+#  index_chatroom_users_on_user_id_and_chatroom_id  (user_id,chatroom_id) UNIQUE
 #
 # Foreign Keys
 #
@@ -20,7 +20,7 @@
 #  fk_rails_...  (user_id => users.id)
 #
 FactoryBot.define do
-  factory :join_chatroom do
+  factory :chatroom_users do
     user { nil }
     chatroom { nil }
   end
